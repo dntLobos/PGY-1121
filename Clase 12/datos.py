7319599dfeefb09c8eb7ba7dc249be803b2c0a99
dntLobos <134552793+dntLobos@users.noreply.github.com>
2023-05-24 16:42:07 -0400

    Add files via upload

---
+++ Clase 12/datos.py
@@ -0,0 +1,9 @@
+# EJEMPLOS DE USO DE GITHUB
+print("Ingreso de Datos")
+print("----------------/n")
+vnom = input("Ingrese su Nombre: ")
+vedad = input("Ingrese su Edad: ")
+print("--------------------------")
+print(f"Su Nombre es: {vnom}")
+print(f"Su edad es: {vedad}")
+print("Programa finalizado")
